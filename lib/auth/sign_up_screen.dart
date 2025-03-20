5a08c7566671cd39e3943f8c5e38e474cd598e18
mmigur <mmigur@bk.ru>
2025-03-20 19:20:14 +0600

    <add>: add sign in screen

--- lib/auth/sign_up_screen.dart
+++ lib/auth/sign_up_screen.dart
@@ -1,10 +1,189 @@
 import 'package:flutter/material.dart';
+import 'package:golem/auth/sign_in_screen.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
   @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isEmailValid = false;
+  bool _isPasswordValid = false;
+  bool _isPasswordVisible = false;
+
+
+  void _validateEmail() {
+    final email = _emailController.text;
+    final regex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    setState(() {
+      _isEmailValid = regex.hasMatch(email);
+    });
+  }
+
+  void _validatePassword() {
+    final password = _passwordController.text;
+    final regex = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$');
+    setState(() {
+      _isPasswordValid = regex.hasMatch(password);
+    });
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordVisible = !_isPasswordVisible;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController.addListener(_validateEmail);
+    _passwordController.addListener(_validatePassword);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Вход',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Почта',
+                    style: TextStyle(color: Color(0xFF80858F)),
+                  )
+                ],
+              ),
+              const SizedBox(height: 8),
+              Container(
+                height: 48.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF8F8F8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'example@gmail.com',
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
+                    border: InputBorder.none,
+                    hintStyle: const TextStyle(color: Color(0xFF80858F)),
+                  ),
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+              if (!_isEmailValid && _emailController.text.isNotEmpty)
+                const Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Введите корректный email',
+                        style: TextStyle(color: Colors.red),
+                      )
+                    ],
+                  ),
+                ),
+              const SizedBox(height: 8.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Пароль',
+                    style: TextStyle(color: Color(0xFF80858F)),
+                  )
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              Container(
+                height: 48.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF8F8F8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    hintText: 'Введите пароль',
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
+                    border: InputBorder.none,
+                    hintStyle: const TextStyle(color: Color(0xFF80858F)),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: const Color(0xFF80858F),
+                      ),
+                      onPressed: _togglePasswordVisibility,
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              SizedBox(
+                width: double.infinity,
+                height: 48.0,
+                child: ElevatedButton(
+                  onPressed: _isEmailValid && _isPasswordValid
+                      ? () {
+                    // Действие при нажатии на кнопку подтверждения почты
+                  }
+                      : null,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _isEmailValid && _isPasswordValid ? Colors.black : const Color(0xFFD9D9D9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'Подтвердить почту',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Нет аккаунта ?',
+                    style: TextStyle(color: Color(0xFF80858F)),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => SignInScreen()),
+                      );
+                    },
+                    child: const Text(
+                      'Регистрация',
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
